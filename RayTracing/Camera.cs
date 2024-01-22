18755f6bb9b475c91411e3aadee6f99eae378ac3
DanilaSysoev <danilasysoev@danilasysoev.ru>
2024-01-22 23:38:19 +0300

    Добавил камеру

---
+++ RayTracing/Camera.cs
@@ -0,0 +1,11 @@
+namespace RayTracing;
+
+public class Camera
+{
+    public Vector3 Position { get; set; } = Vector3.Zero;
+    public Vector3 ViewPoint { get; set; } = new Vector3(0, 0, 1);
+    public Vector3 Up { get; set; } = new Vector3(0, 1, 0);
+
+    public double FieldOfView { get; set; } = 90;
+    public double Aspect { get; set; } = 16.0 / 9.0;
+}
