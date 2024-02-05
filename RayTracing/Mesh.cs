d21153241ec0db6105c2163c21ab3b41d027f8d3
DanilaSysoev <danilasysoev@danilasysoev.ru>
2024-02-05 22:36:38 +0300

    Дополнил меш

--- RayTracing/Mesh.cs
+++ RayTracing/Mesh.cs
@@ -14,7 +14,7 @@ public class Mesh : ISurface
     public Vector3? Intersection(Ray ray)
     {
         if(!ray.Intersect(BoundingBox))
-            return null;        
+            return null;
     }
 
     public Vector3? NormalInIntersection(Ray ray)
@@ -32,14 +32,11 @@ public class Mesh : ISurface
     {
         BoundingBox.Min.Rotate(axis, angleDegree);
         BoundingBox.Max.Rotate(axis, angleDegree);
-        for(int i = 0; i < vertexStorage.Length; ++i)
-        {
-            var normalPoint = vertexStorage[i] + normalStorage[i];
-            normalPoint.Rotate(axis, angleDegree);
+        for(int i = 0; i < vertexStorage.Count; ++i)
             vertexStorage[i].Rotate(axis, angleDegree);
-            normalStorage[i] = (normalPoint - vertexStorage[i]).Normalize();
-        }
-        
+        for(int i = 0; i < normalStorage.Count; ++i)
+            normalStorage[i].Rotate(axis, angleDegree);
+                    
         RebuildTriangles();
         RebuildNormals();
     }
@@ -48,7 +45,7 @@ public class Mesh : ISurface
     {
         BoundingBox.Min.Scale(axis, scaleFactor);
         BoundingBox.Max.Scale(axis, scaleFactor);
-        for(int i = 0; i < vertexStorage.Length; ++i)
+        for(int i = 0; i < vertexStorage.Count; ++i)
             vertexStorage[i].Scale(axis, scaleFactor);
 
         RebuildTriangles();
@@ -58,16 +55,52 @@ public class Mesh : ISurface
     {
         BoundingBox.Min.Translate(on);
         BoundingBox.Max.Translate(on);
-        for(int i = 0; i < vertexStorage.Length; ++i)
+        for(int i = 0; i < vertexStorage.Count; ++i)
             vertexStorage[i].Translate(on);
 
         RebuildTriangles();
     }
 
+    public void AddVertex(Vector3 vertex)
+    {
+        vertexStorage.Add(vertex);
+    }
+    public void AddNormal(Vector3 normal)
+    {
+        normalStorage.Add(normal);
+    }
+    public void AddTextureUV(TextureUV textureUV)
+    {
+        texturUvsStorage.Add(textureUV);
+    }
+    public void AddFace(Face face)
+    {
+        faces.Add(face);
+        triangles.Add(new Triangle(vertexStorage[face.Va],
+                                   vertexStorage[face.Vb],
+                                   vertexStorage[face.Vc]));
+        if(face.Na is not null)
+        {
+            normals.Add(new ItemSet<Vector3>{
+                A = normalStorage[face.Na!.Value],
+                B = normalStorage[face.Nb!.Value],
+                C = normalStorage[face.Nc!.Value]
+            });
+        }
+        if(face.Ta is not null)
+        {
+            textureUvs.Add(new ItemSet<TextureUV>{
+                A = texturUvsStorage[face.Ta!.Value],
+                B = texturUvsStorage[face.Tb!.Value],
+                C = texturUvsStorage[face.Tc!.Value]
+            });
+        }
+    }
+
 
     private void RebuildTriangles()
     {
-        for(int i = 0; i < faces.Length; ++i)
+        for(int i = 0; i < faces.Count; ++i)
         {
             triangles[i] = new Triangle(vertexStorage[faces[i].Va],
                                         vertexStorage[faces[i].Vb],
@@ -78,7 +111,7 @@ public class Mesh : ISurface
     {
         if(!faces[0].Na.HasValue)
             return;
-        for(int i = 0; i < faces.Length; ++i)
+        for(int i = 0; i < faces.Count; ++i)
         {
             normals[i] = new ItemSet<Vector3>{
                 A = normalStorage[faces[i].Na!.Value],
@@ -91,7 +124,7 @@ public class Mesh : ISurface
     {
         if(!faces[0].Ta.HasValue)
             return;
-        for(int i = 0; i < faces.Length; ++i)
+        for(int i = 0; i < faces.Count; ++i)
         {
             textureUvs[i] = new ItemSet<TextureUV>{
                 A = texturUvsStorage[faces[i].Ta!.Value],
@@ -101,25 +134,25 @@ public class Mesh : ISurface
         }        
     }
 
-    private Vector3[] vertexStorage;
-    private Vector3[] normalStorage;
-    private TextureUV[] texturUvsStorage;
-    private Face[] faces;
+    private List<Vector3> vertexStorage;
+    private List<Vector3> normalStorage;
+    private List<TextureUV> texturUvsStorage;
+    private List<Face> faces;
 
-    private Triangle[] triangles;
-    private ItemSet<Vector3>[] normals;
-    private ItemSet<TextureUV>[] textureUvs;
+    private List<Triangle> triangles;
+    private List<ItemSet<Vector3>> normals;
+    private List<ItemSet<TextureUV>> textureUvs;
 
-    private Mesh(int vertexCount, int normalsCount, int texesCount, int facesCount)
+    public Mesh()
     {
-        vertexStorage = new Vector3[vertexCount];
-        normalStorage = new Vector3[normalsCount];
-        texturUvsStorage = new TextureUV[texesCount];
+        vertexStorage = new List<Vector3>();
+        normalStorage = new List<Vector3>();
+        texturUvsStorage = new List<TextureUV>();
 
-        faces = new Face[facesCount];
-        triangles = new Triangle[facesCount];
-        normals = new ItemSet<Vector3>[facesCount];
-        textureUvs = new ItemSet<TextureUV>[facesCount];
+        faces = new List<Face>();
+        triangles = new List<Triangle>();
+        normals = new List<ItemSet<Vector3>>();
+        textureUvs = new List<ItemSet<TextureUV>>();
 
         BoundingBox = new BoundingBox();
     }
