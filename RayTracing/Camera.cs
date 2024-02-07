1f523bb495f7b2ac31874fef01be367e958b7cd1
DanilaSysoev <danilasysoev@danilasysoev.ru>
2024-02-07 17:42:41 +0300

    Добавил генерацию экрана по камере

--- RayTracing/Camera.cs
+++ RayTracing/Camera.cs
@@ -8,4 +8,30 @@ public class Camera
 
     public double FieldOfView { get; set; } = 90;
     public double Aspect { get; set; } = 16.0 / 9.0;
+
+    public Screen GetScreen(int width, int height)
+    {        
+        var direction = (ViewPoint - Position).Normalized;
+        var up = Up.Normalized;
+        var right = direction.Cross(up);
+        var center = Position + direction * ScreenDistance;
+        var x = ScreenDistance * Math.Tan(FieldOfView * Math.PI / 360);
+        var Rc = center + right * x;
+        var Lc = center - right * x;
+        var y = x / Aspect;
+        var Lu = Lc + up * y;
+        var Ld = Lc - up * y;
+        var Ru = Rc + up * y;
+        
+        return new Screen
+        {
+            Width = width,
+            Height = height,
+            LeftUp = Lu,
+            LeftDown = Ld,
+            RightUp = Ru
+        };
+    }
+
+    public const double ScreenDistance = 1.0;
 }
